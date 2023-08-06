90421e839734bf69798f4a8cb4a7d71984898570
yukselozcan <yuksel.ozcan@student.griffith.ie>
2023-08-06 18:08:29 +0800

    commit with successful workout adapter etc

--- app/src/main/java/com/example/gymapplication/activities/workouts/SavedWorkoutActivity.kt
+++ app/src/main/java/com/example/gymapplication/activities/workouts/SavedWorkoutActivity.kt
@@ -4,13 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gymapplication.R
+import com.example.gymapplication.adapters.ExerciseAdapter
+import com.example.gymapplication.adapters.WorkoutAdapter
+import com.example.gymapplication.models.ExerciseModel
+import com.example.gymapplication.models.WorkoutModel
+import com.google.firebase.database.*
 
 private lateinit var btnCreateRoutine: Button
 private lateinit var tvDate : TextView
+private lateinit var rvWorkout:RecyclerView
+private lateinit var dbRef:DatabaseReference
+private lateinit var workoutList:ArrayList<WorkoutModel>
 
-class SavedWorkoutActivity : AppCompatActivity() {
+class SavedWorkoutActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +32,48 @@ class SavedWorkoutActivity : AppCompatActivity() {
         tvDate = findViewById(R.id.tvSelectedDate)
         tvDate.text= date
 
+        rvWorkout=findViewById(R.id.rvSavedWorkouts)
+        rvWorkout.layoutManager=LinearLayoutManager(this)
+        rvWorkout.setHasFixedSize(true)
+        workoutList= arrayListOf()
+        getWorkouts()
+
         btnCreateRoutine.setOnClickListener{
             val intent = Intent(this, CreateWorkoutActivity::class.java )
             startActivity(intent)
         }
+    }
+    private fun getWorkouts(){
+        val customUrl = "https://gymappfirebase-9f06f-default-rtdb.europe-west1.firebasedatabase.app"
+        dbRef = FirebaseDatabase.getInstance(customUrl).getReference("Workouts")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                workoutList.clear()
+                if (snapshot.exists()) {
+                    for (userSnap in snapshot.children) {
+                        val workoutData = userSnap.getValue(WorkoutModel::class.java)
+                        workoutList.add(workoutData!!)
+                        val mAdapter = WorkoutAdapter(workoutList)
+                        rvWorkout.adapter = mAdapter
+                        mAdapter.setOnItemClickListener(object :
+                            WorkoutAdapter.onItemClickListener {
+                            override fun onItemClick(position: Int) {
+
+                                Toast.makeText(
+                                    this@SavedWorkoutActivity,
+                                    "You clicked on ${workoutList[position].workoutName}",
+                                    Toast.LENGTH_LONG
+                                ).show()
+
+                            }
+                        })
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
--- app/src/main/java/com/example/gymapplication/adapters/WorkoutAdapter.kt
+++ app/src/main/java/com/example/gymapplication/adapters/WorkoutAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.gymapplication.adapters
 
-class WorkoutAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gymapplication.R
+import com.example.gymapplication.models.WorkoutModel
+
+class WorkoutAdapter ( private val workoutList: ArrayList<WorkoutModel>)
+    :RecyclerView.Adapter<WorkoutAdapter.ViewHolder>(){
+
+    private var mListener : onItemClickListener?= null
+
+    interface onItemClickListener{
+        fun onItemClick(position:Int)
+    }
+
+    fun setOnItemClickListener(clickListener: onItemClickListener){
+        mListener=clickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup,position: Int):ViewHolder{
+        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.saved_workout_items,parent,false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder:ViewHolder , position:Int){
+        val selectedWorkout = workoutList[position]
+        holder.tv_workout_name.text=selectedWorkout.workoutName
+        holder.itemView.setOnClickListener{
+            mListener?.onItemClick(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return workoutList.size
+    }
+
+    class ViewHolder(itemview:View): RecyclerView.ViewHolder(itemview){
+        val tv_workout_name:TextView= itemview.findViewById(R.id.tv_workout_name)
+    }
+
+
+
 }
